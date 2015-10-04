a65921a45a07a22bcf3cfcbde663984ae4333324
Peter Maurer <p.maurer@maurer-treutner.de>
2015-10-04 19:24:34 +0200

    Some nice modifications...

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -5,3 +5,9 @@
 
 Calc::Calc(int value):_value(value)
 {}
+
+int Calc::add(int sum)
+{
+  _value += sum;
+  return _value;
+}
--- calc/calc.h
+++ calc/calc.h
@@ -8,6 +8,8 @@ public:
   Calc(int value);
   virtual ~Calc(){};
 
+  virtual int add(int sum);
+
 private:
   int _value;
 };
